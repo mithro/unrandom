1f34792168b7074c449e19c7cb65a24a6e335c95
Tim 'mithro' Ansell <mithro@mithis.com>
2016-03-30 01:43:12 +1100

    open has vaargs

--- unrandom.c
+++ unrandom.c
@@ -3,6 +3,7 @@
 #include <dlfcn.h>
 #include <stdlib.h>
 #include <assert.h>
+#include <stdarg.h>
 
 static void (*orig_srand)(unsigned int seed);
 
@@ -18,13 +19,24 @@ void srand(unsigned int seed) {
   orig_srand(v?atoi(v):0);
 }
 
-static int (*orig_open)(const char *pathname, int flags, mode_t mode);
+static int (*orig_open)(const char *pathname, int flags, ...);
 
-int open(const char *pathname, int flags, mode_t mode) {
+int open(const char *pathname, int flags, ...) {
+  va_list ap;
+  mode_t mode;
+
   if(!orig_open) {
     orig_open = dlsym(RTLD_NEXT, "open");
     assert(orig_open);
   }
+
+  va_start(ap, flags);
+#if SIZEOF_MODE_T < SIZEOF_INT
+  mode= (mode_t) va_arg(ap, int);
+#else
+  mode= va_arg(ap, mode_t);
+#endif
+  va_end(ap);
 
   if (strcmp(pathname, "/dev/urandom", mode) == 0) {
     return orig_open("/dev/zero", flags, mode);
